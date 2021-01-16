c31f7144a2b33b237e88f9dcefa6e0c215ade25e
Jordan Morse <jordanmorse@Jordans-MBP-2.attlocal.net>
2021-01-16 14:58:25 -0500

    fixing runtime cache

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+const URL = process.env.MONGODB_URI || "mongodb://localhost/budget"
+
+mongoose.connect(URL, {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+})
+
 
 // routes
 app.use(require("./routes/api.js"));
